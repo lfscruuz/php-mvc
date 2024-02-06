4c397667beeab49121681aff50e08b39706dd2c0
Luis Felipe da Silva Cruz <luis.cruz@infrasa.gov.br>
2024-02-06 14:02:15 -0300

    around the 50 min mak

--- App/Http/Router.php
+++ App/Http/Router.php
@@ -2,6 +2,7 @@
 
 namespace App\http;
 use \Closure;
+use \Exception;
 
 class Router{
     private $url = '';
@@ -12,17 +13,15 @@ class Router{
     public function __construct($url){
         $this->request = new Request();
         $this->url = $url;
+        $this->setPrefix();
     }
 
-    private function setPrefix($url){
+    private function setPrefix(){
         $parseUrl = parse_url($this->url);
         $this->prefix = $parseUrl['path'] ?? '';
     }
 
     private function addRoute($method, $route, $params = []){
-        echo "<pre>";
-        print_r($params);
-        echo "<pre>";
         foreach($params as $key => $value){
             if ($value instanceof Closure){
                 $params['controller'] = $value;
@@ -30,14 +29,52 @@ class Router{
                 continue;
             }
         }
-        echo "<pre>";
-        print_r($params);
-        echo "<pre>";
-        exit;
-    }
 
+        $patternRoute = "/^".str_replace("/", "\/", $route)."$/";
+        $this->routes[$patternRoute][$method] = $params;
+
+        // echo "<pre>";
+        // print_r($this);
+        // echo "</pre>";
+        // exit;
+    }
+    
     public function get($route, $params = []){
         return $this->addRoute('GET', $route, $params);
+    }
+    
+    private function getUri(){
+        $uri = $this->request->getUri();
+        $xUri = strlen($this->prefix) ? explode($this->prefix, $uri) : [$uri];
+        return end($xUri);
+    }
+    private function getRoute(){
+        $uri = $this->getUri();
+        $httpMethod = $this->request->getHttpMethod();
+
+        foreach($this->routes as $patternRoute => $methods){
+            if(preg_match($patternRoute, $uri)){
+                if($methods[$httpMethod]){
+                    return $methods[$httpMethod];
+                }
+            }
+
+            throw new Exception ("Método não permitido", 405);
+        }
+
+        throw new Exception ("URL não encontrada",404);
+    }
+
+    public function run(){
+        try{
+            $route = $this->getRoute();
+            echo "<pre>";
+            print_r($route);
+            echo "</pre>";
+            exit;
+        } catch (Exception $e){
+            return new Response($e->getCode(), $e->getMessage());
+        }
     }
 }
 
--- index.php
+++ index.php
@@ -15,3 +15,4 @@ $obRouter->get('/',[
     }
 ]);
 
+$obRouter->run();
